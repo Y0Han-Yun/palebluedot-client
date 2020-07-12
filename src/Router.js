725e97638b867283048d51084112c64085234528
Hyun Ho Oh <ohhobs@gmail.com>
2020-07-12 21:43:01 +1000

    reformat document (indentation & spacing) for Router.js

--- src/Router.js
+++ src/Router.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Setting from './pages/Setting';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    body {
-        margin: 0px;
-    }
+  body {
+    margin: 0px;
+  }
 `;
 
-class Router extends React.Component{
-    render (){
-        return(<>
-        <GlobalStyle/>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route exact path="regi">
-                    <Register/>
-                </Route>
-                <Route exact path="Setting">
-                    <Setting/>
-                </Route>
-            </Switch>
-        </BrowserRouter>
-        </>);
-    }
+class Router extends React.Component {
+  render() {
+    return (<>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="regi">
+            <Register />
+          </Route>
+          <Route exact path="Setting">
+            <Setting />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>);
+  }
 }
 export default Router;
