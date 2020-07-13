c707b98cefd09c54def7338de7b1f580d7b92de7
Yo Han Yoon <yyh1283@gmail.com>
2020-07-14 05:47:26 +1000

    made button component

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Button = styled.button`
+  type: primary;
 
-class Button extends React.Component{
-    render(){
-        return(<>
+`;
+
+const Button = styled.button`
+  type: secondary;
+  
+`;
+
+class Button extends React.Component {
+    render() {
+        return (<>
           <Button type="primary">Click me!</Button>
           <Button type="secondary">Click me!</Button>
         </>);
     }
 }
+
 export default Button;
