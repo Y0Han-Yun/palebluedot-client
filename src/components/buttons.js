ecccf4ad97bdc5cce4867fd2ba188543096884d1
Yo Han Yoon <yyh1283@gmail.com>
2020-07-13 21:58:35 +1000

    Done

---
+++ src/components/buttons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Button extends React.Component{
+    render(){
+        return(<>
+          <Button type="primary">Click me!</Button>
+          <Button type="secondary">Click me!</Button>
+        </>);
+    }
+}
+export default Button;
