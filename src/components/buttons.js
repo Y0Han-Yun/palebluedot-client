4b0415c534813cf285a99c6bdc4d4a8bcb29b805
Hyun Ho Oh <ohhobs@gmail.com>
2020-07-14 12:20:05 +1000

    refactor button component file name and to allow pass children

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const StyledComponent = styled.button``;
 
+Button.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
+};
+
 class Button extends React.Component {
 
   render () {
-    return <StyledComponent></StyledComponent>;
+    return <StyledComponent>{this.props.children}</StyledComponent>;
   }
 
 }
