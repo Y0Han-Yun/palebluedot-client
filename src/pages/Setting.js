fda2c3ec339cd9467ff4a99edb3aa55d7dce210d
Yo Han Yoon <yyh1283@gmail.com>
2020-07-16 06:59:18 +1000

    Working on the Setting page

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,6 +1,29 @@
 import { withRouter } from 'react-router-dom';
 import React from 'react';
+import styled from 'styled-components';
 import Navigation from '../components/Navigation';
+
+const Container = styled.div`
+  background: powderblue;
+  width: 1440px;
+  height: 790px;
+  margin: 0px;
+  border: 0px;
+  padding: 0px;
+  display: flex;
+`;
+
+const SetPage = styled.div`
+  width: 80%;
+  height: 90%;
+  margin: 0px;
+  border: 0px;
+  padding: 0px;
+  display: flex;
+`;
+
+
+
 
 
 class Setting extends React.Component {
@@ -8,6 +31,10 @@ class Setting extends React.Component {
   render () {
     return (<>
       <Navigation/>
+      <Container>
+        <SetPage></SetPage>
+
+      </Container>
     </>);
   }
 
