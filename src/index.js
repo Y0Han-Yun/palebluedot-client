8f7bd32a0543bf7b59c8ac6a01ef49e9e74e6603
Yo Han Yoon <yyh1283@gmail.com>
2020-07-25 12:43:32 +1000

    123123

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Router from './Router';
 console.log('Hello');
 console.log('hi');
 console.log('123');
+console.log('yohan12');
 console.log('yohan');
 
 ReactDOM.render(<Router/>, document.querySelector('#root'));
