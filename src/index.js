896e4bace57a5e9e9b7c436088fbebbc467a0153
Hyun Oh <hyun.oh@austcommunitymedia.com.au>
2020-07-25 12:49:03 +1000

    test123

--- src/index.js
+++ src/index.js
@@ -5,11 +5,8 @@ import Router from './Router';
 console.log('Hello');
 console.log('hi');
 console.log('123');
-<<<<<<< HEAD
 console.log('yohan!!');
-=======
 console.log('yohan12');
 console.log('yohan');
->>>>>>> 8f7bd32a0543bf7b59c8ac6a01ef49e9e74e6603
 
 ReactDOM.render(<Router/>, document.querySelector('#root'));
