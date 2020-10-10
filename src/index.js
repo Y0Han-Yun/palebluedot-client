fa3f95130923bd073d5b63bc77d1d3b41e54b497
Yo Han Yoon <yyh1283@gmail.com>
2020-10-10 13:35:29 +1100

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Version 2</h1>, document.querySelector('#root'));
+ReactDOM.render(<h1>Version 2 editied</h1>, document.querySelector('#root'));
