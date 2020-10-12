258648026e7959de879539c626b3462307bf2025
Yo Han Yoon <yyh1283@gmail.com>
2020-10-13 05:03:09 +1100

    add a image

--- src/Pages/Web/Skills/index.js
+++ src/Pages/Web/Skills/index.js
@@ -4,12 +4,38 @@ import BackGround from '/Users/yoonyohan/Desktop/project/palebluedot-client/src/
 
 const StyledComponent = styled.div``;
 
+const BackGroudImage = styled.img`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: auto;
+  opacity: 0.6;
+  object-fit: cover;
+  object-position: 50% 60%;  
+`;
+
+const Zindex1 = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
+  -webkit-overflow-scrolling: touch;
+  z-index: 1;
+`;
+
 class Skills extends React.Component {
 
   render () {
     return (
       <StyledComponent>
-        <img src={BackGround} alt='Skills' />
+        <BackGroudImage src={BackGround} alt="Skills" />
+        <Zindex1>
+          <div>asdfsadf</div>
+        </Zindex1>
       </StyledComponent>
     );
   }
