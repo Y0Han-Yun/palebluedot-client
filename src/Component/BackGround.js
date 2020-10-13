49b0c18b0865f0b120e0169da519e28061c49fdc
Yo Han Yoon <yyh1283@gmail.com>
2020-10-13 19:49:11 +1100

    add BackGround Component

--- src/Component/BackGround.js
+++ src/Component/BackGround.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import BackGroundImage from '/Users/yoonyohan/Desktop/project/palebluedot-client/src/Images/BackGroundImage.jpg';
 
-const StyledComponent = styled.img`
+const StyledComponent = styled.div``;
+const Image = styled.img`
   position: fixed;
   top: 0;
   left: 0;
@@ -11,31 +12,33 @@ const StyledComponent = styled.img`
   z-index: auto;
   opacity: 0.6;
   object-fit: cover;
-  object-position: 50% 60%;  
-
-  .Z-index1 {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: scroll;
-    -webkit-overflow-scrolling: touch;
-    z-index: 1;
-  }
+  object-position: 50% 60%;
 `;
 
+const Zindex1 = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
+  -webkit-overflow-scrolling: touch;
+  z-index: 1;
+`;
 
 class BackGround extends React.Component {
+
   render () {
     return (<>
-      <StyledComponent src={BackGroundImage} alt="BackGouund">
-         <div className="Z-index1">
-           {this.props.children}
-         </div>
+      <StyledComponent >
+        <Image src={BackGroundImage} alt="BackGouund" />
+          <Zindex1>
+            {this.props.children}
+          </Zindex1>
       </StyledComponent>
     </>);
   }
+
 }
 
 export default BackGround;
