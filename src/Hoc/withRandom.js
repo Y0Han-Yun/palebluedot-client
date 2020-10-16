f0aa20fe37b1b18459ff4c079202e306997fe9df
Hyun Oh <hyun.oh@austcommunitymedia.com.au>
2020-10-16 21:51:43 +1100

    Fri 16 Oct 2020 21:51:43 AEDT

--- src/Hoc/withRandom.js
+++ src/Hoc/withRandom.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const withRandom = WrappedComponent => {
   return class extends React.Component {
+
     render () {
       // {...this.props} => WrappedComponent를 실제로 사용하는 부모에서 내려준 props
       return <WrappedComponent generate={() => {
         const min = 0;
         const max = 100;
         return Math.floor(Math.random() * (max - min + 1) + min);
-      }} {...this.props} />
+      }} {...this.props} />;
     }
+
   };
 };
 
