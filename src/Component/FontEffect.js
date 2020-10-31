0a83ad7a076ceb704dca425b22857fa8d27723cd
Yo Han Yoon <yyh1283@gmail.com>
2020-11-01 08:33:42 +1100

    asdf

--- src/Component/FontEffect.js
+++ src/Component/FontEffect.js
@@ -12,14 +12,12 @@ const StyledComponent = styled.div`
 
 `;
 
-
-
 class FontEffect extends React.Component {
 
   render () {
     return (
       <StyledComponent>
-            {this.props.children}
+        {this.props.children}
       </StyledComponent>
     );
   }
